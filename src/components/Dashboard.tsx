e0d7bdaea575b78adf079329271dfc002a48165d
varun.nayak <varunrunrunrun@gmail.com>
2024-08-01 05:07:42 -0700

    initiated the implementation of dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,8 +2,9 @@
 
 const Dashboard = () => {
     return (
-        <div className="flex flex-1 flex-col p-6 bg-primary text-white pt-24">
+        <div className="flex flex-1 flex-col gap-4 p-6 bg-primary text-white pt-24">
             <h1 className="text-2xl font-semibold">Dashboard</h1>
+
         </div>
     )
 }
